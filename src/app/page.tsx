50305156c7eb65ff2c334114aac51ea87fc9f42e
jkhall81 <jason.kei.hall@gmail.com>
2024-12-06 09:28:59 -0700

    first commit

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-center pt-4 font-semibold text-2xl">
+        Documentation Project
+      </h1>
+    </div>
+  );
+}
